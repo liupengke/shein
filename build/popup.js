48c54689995f9bb990f84c42fde1f7afb016d04f
liupengke <liupengke@inagora.cn>
2023-12-21 10:20:10 +0800

    init

---
+++ build/popup.js
@@ -0,0 +1,209 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/popup.css":
+/*!***********************!*\
+  !*** ./src/popup.css ***!
+  \***********************/
+/***/ (() => {
+
+// extracted by mini-css-extract-plugin
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
+"use strict";
+/*!**********************!*\
+  !*** ./src/popup.js ***!
+  \**********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _popup_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./popup.css */ "./src/popup.css");
+/* harmony import */ var _popup_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_popup_css__WEBPACK_IMPORTED_MODULE_0__);
+
+
+
+
+(function() {
+  // We will make use of Storage API to get and store `count` value
+  // More information on Storage API can we found at
+  // https://developer.chrome.com/extensions/storage
+
+  // To get storage access, we have to mention it in `permissions` property of manifest.json file
+  // More information on Permissions can we found at
+  // https://developer.chrome.com/extensions/declare_permissions
+  const counterStorage = {
+    get: cb => {
+      chrome.storage.sync.get(['count'], result => {
+        cb(result.count);
+      });
+    },
+    set: (value, cb) => {
+      chrome.storage.sync.set(
+        {
+          count: value,
+        },
+        () => {
+          cb();
+        }
+      );
+    },
+  };
+
+  function setupCounter(initialValue = 0) {
+    document.getElementById('counter').innerHTML = initialValue;
+
+    document.getElementById('incrementBtn').addEventListener('click', () => {
+      updateCounter({
+        type: 'INCREMENT',
+      });
+    });
+
+    document.getElementById('decrementBtn').addEventListener('click', () => {
+      updateCounter({
+        type: 'DECREMENT',
+      });
+    });
+  }
+
+  function updateCounter({ type }) {
+    counterStorage.get(count => {
+      let newCount;
+
+      if (type === 'INCREMENT') {
+        newCount = count + 1;
+      } else if (type === 'DECREMENT') {
+        newCount = count - 1;
+      } else {
+        newCount = count;
+      }
+
+      counterStorage.set(newCount, () => {
+        document.getElementById('counter').innerHTML = newCount;
+
+        // Communicate with content script of
+        // active tab by sending a message
+        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+          const tab = tabs[0];
+
+          chrome.tabs.sendMessage(
+            tab.id,
+            {
+              type: 'COUNT',
+              payload: {
+                count: newCount,
+              },
+            },
+            response => {
+              console.log('Current count value passed to contentScript file');
+            }
+          );
+        });
+      });
+    });
+  }
+
+  function restoreCounter() {
+    // Restore count value
+    counterStorage.get(count => {
+      if (typeof count === 'undefined') {
+        // Set counter value as 0
+        counterStorage.set(0, () => {
+          setupCounter(0);
+        });
+      } else {
+        setupCounter(count);
+      }
+    });
+  }
+
+  document.addEventListener('DOMContentLoaded', restoreCounter);
+
+  // Communicate with background file by sending a message
+  chrome.runtime.sendMessage(
+    {
+      type: 'GREETINGS',
+      payload: {
+        message: 'Hello, my name is Pop. I am from Popup.',
+      },
+    },
+    response => {
+      console.log(response.message);
+    }
+  );
+})();
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=popup.js.map
